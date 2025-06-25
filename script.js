c37bbade30a0ed4eb1e79b047332d536d97a419c
DalaScript <nik.dalalishvili@gmail.com>
2025-06-25 15:25:08 +0400

    add favorites DOM elements

--- script.js
+++ script.js
@@ -6,52 +6,55 @@ const loader = document.querySelector('.loader');
 
 // NASA API
 const count = 10;
-const apiKey = 'DEMO_KEY';
+// const apiKey = 'DEMO_KEY';
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
 let favorites = {};
 
-function updateDOM() {
-    resultsArray.forEach((result) => {
+function createDOMNodes(page) {
+    const currentArray = page === 'results' ? resultsArray : Object.values(favorites);
+    console.log('Current Array', page, currentArray);
+    currentArray.forEach((result) => {
         // Card Container
-        const card = document.createElement('div');
-        card.classList.add('card');
+        const card = document.createElement("div");
+        card.classList.add("card");
         // Link
-        const link = document.createElement('a');
+        const link = document.createElement("a");
         link.href = result.hdurl;
-        link.title = 'View Full Image';
-        link.target = '_blank';
+        link.title = "View Full Image";
+        link.target = "_blank";
         // Image
-        const image = document.createElement('img');
+        const image = document.createElement("img");
         image.src = result.url;
-        image.alt = 'NASA Picture of the Day';
-        image.loading = 'lazy';
-        image.classList.add('card-img-top');
+        image.alt = "NASA Picture of the Day";
+        image.loading = "lazy";
+        image.classList.add("card-img-top");
         // Card Body
-        const cardBody = document.createElement('div');
-        cardBody.classList.add('card-body');
+        const cardBody = document.createElement("div");
+        cardBody.classList.add("card-body");
         // Card Title
-        const cardTitle = document.createElement('h5');
-        cardTitle.classList.add('card-title');
+        const cardTitle = document.createElement("h5");
+        cardTitle.classList.add("card-title");
         cardTitle.textContent = result.title;
         // Save Text
-        const saveText = document.createElement('p');
-        saveText.classList.add('clickable');
-        saveText.textContent = 'Add To Favorites';
-        saveText.setAttribute('onClick', `saveFavorite('${result.url}')`);
+        const saveText = document.createElement("p");
+        saveText.classList.add("clickable");
+        saveText.textContent = "Add To Favorites";
+        saveText.setAttribute("onClick", `saveFavorite('${result.url}')`);
         // Card Text
-        const cardText = document.createElement('p');
+        const cardText = document.createElement("p");
         cardText.textContent = result.explanation;
         // Footer Container
-        const footer = document.createElement('small');
-        footer.classList.add('text-muted');
+        const footer = document.createElement("small");
+        footer.classList.add("text-muted");
         // Date
-        const date = document.createElement('strong');
+        const date = document.createElement("strong");
         date.textContent = result.date;
         // Copyright
-        const copyrightResult = result.copyright === undefined ? '' : result.copyright
-        const copyright = document.createElement('span');
+        const copyrightResult =
+            result.copyright === undefined ? "" : result.copyright;
+        const copyright = document.createElement("span");
         copyright.textContent = ` ${copyrightResult}`;
         // Append
         footer.append(date, copyright);
@@ -62,13 +65,21 @@ function updateDOM() {
     });
 }
 
+function updateDOM(page) {
+    // Get Favorites from localStorage
+    if (localStorage.getItem('nasaFavorites')) {
+        favorites = JSON.parse(localStorage.getItem('nasaFavorites'));
+        console.log('favorites from localStorage', favorites);
+    }
+    createDOMNodes(page);
+}
+
 // Get 10 Images from NASA API
 async function getNasaPictures() {
     try {
         const response = await fetch(apiUrl);
         resultsArray = await response.json();
-        console.log(resultsArray);
-        updateDOM();
+        updateDOM('favorites');
     } catch (error) {
         // Catch Error Here
         console.log(error);
